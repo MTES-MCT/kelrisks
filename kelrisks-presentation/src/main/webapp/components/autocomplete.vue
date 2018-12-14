88f9700152435157e9bafe9e4b0f49fa84c6933e
thomas.bouchardon <thomas.bouchardon@developpement-durable.gouv.fr>
2018-12-14 11:43:52 +0100

    Init Vue

--- kelrisks-presentation/src/main/webapp/components/autocomplete.vue
+++ kelrisks-presentation/src/main/webapp/components/autocomplete.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="autocomplete"><input @input="onChange"
-                                     @keydown.down="onArrowDown"
                                      @keydown.enter="onEnter"
-                                     @keydown.up="onArrowUp"
                                      type="text"
+                                     @keydown.down="onArrowDown"
+                                     @keydown.up="onArrowUp"
                                      v-model="search"/>
         <ul class="autocomplete-results"
             id="autocomplete-results"
@@ -12,15 +12,133 @@
                 v-if="isLoading">Loading results...
             </li>
             <li :class="{ 'is-active': i === arrowCounter }"
+                v-else
                 :key="i"
                 @click="setResult(result)"
                 class="autocomplete-result"
-                v-else
                 v-for="(result, i) in results">{{ result }}
             </li>
         </ul>
     </div>
 </template>
 
-<script src="autocomplete.js"></script>
-<script src="autocomplete.css"></script>
+<script>
+    export default {
+        name: 'autocomplete',
+
+        props: {
+            items: {
+                type: Array,
+                required: false,
+                default: () => [],
+            },
+            isAsync: {
+                type: Boolean,
+                required: false,
+                default: false,
+            },
+        },
+
+        data() {
+            return {
+                isOpen: false,
+                results: [],
+                search: '',
+                isLoading: false,
+                arrowCounter: 0,
+            };
+        },
+
+        methods: {
+            onChange() {
+                // Let's warn the parent that a change was made
+                this.$emit('input', this.search);
+
+                // Is the data given by an outside ajax request?
+                if (this.isAsync) {
+                    this.isLoading = true;
+                } else {
+                    // Let's  our flat array
+                    this.filterResults();
+                    this.isOpen = true;
+                }
+            },
+
+            filterResults() {
+                // first uncapitalize all the things
+                this.results = this.items.filter((item) => {
+                    return item.toLowerCase().indexOf(this.search.toLowerCase()) > -1;
+                });
+            },
+            setResult(result) {
+                this.search = result;
+                this.isOpen = false;
+            },
+            onArrowDown(evt) {
+                if (this.arrowCounter < this.results.length) {
+                    this.arrowCounter = this.arrowCounter + 1;
+                }
+            },
+            onArrowUp() {
+                if (this.arrowCounter > 0) {
+                    this.arrowCounter = this.arrowCounter - 1;
+                }
+            },
+            onEnter() {
+                this.search = this.results[this.arrowCounter];
+                this.isOpen = false;
+                this.arrowCounter = -1;
+            },
+            handleClickOutside(evt) {
+                if (!this.$el.contains(evt.target)) {
+                    this.isOpen = false;
+                    this.arrowCounter = -1;
+                }
+            }
+        },
+        watch: {
+            items: function (val, oldValue) {
+                // actually compare them
+                if (val.length !== oldValue.length) {
+                    this.results = val;
+                    this.isLoading = false;
+                }
+            },
+        },
+        mounted() {
+            document.addEventListener('click', this.handleClickOutside)
+        },
+        destroyed() {
+            document.removeEventListener('click', this.handleClickOutside)
+        }
+    };
+</script>
+
+<style>
+    .autocomplete {
+        position : relative;
+    }
+    
+    .autocomplete-results {
+        padding  : 0;
+        margin   : 0;
+        border   : 1px solid #EEEEEE;
+        height   : 120px;
+        overflow : auto;
+        width    : 100%;
+    }
+    
+    .autocomplete-result {
+        list-style : none;
+        text-align : left;
+        padding    : 4px 2px;
+        cursor     : pointer;
+    }
+    
+    .autocomplete-result.is-active,
+    .autocomplete-result:hover {
+        background-color : #4AAE9B;
+        color            : white;
+    }
+
+</style>
