085fa107a83ac49e31dfa84a34c7ef7d3b912aa6
thomas.bouchardon <thomas.bouchardon@developpement-durable.gouv.fr>
2019-07-22 09:46:41 +0200

    Migration Basol

--- kelrisks-facade/src/main/java/fr/gouv/beta/fabnum/kelrisks/facade/dto/referentiel/SiteIndustrielBasolDTO.java
+++ kelrisks-facade/src/main/java/fr/gouv/beta/fabnum/kelrisks/facade/dto/referentiel/SiteIndustrielBasolDTO.java
@@ -6,16 +6,12 @@ import lombok.Data;
 public class SiteIndustrielBasolDTO {
     
     private Long   id;
-    private String numerosite;
     private String numerobasol;
-    private String numeros3ic;
     private String identifiantbasias;
     private String adresse;
     private String commune;
     private String codeinsee;
-    private String codepostal;
     private String proprietaire;
     private String ewkt;
-    private String responsable;
 }
   
