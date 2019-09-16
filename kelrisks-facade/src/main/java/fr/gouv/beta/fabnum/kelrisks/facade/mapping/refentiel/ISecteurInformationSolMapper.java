9df519d5e6c97b930ae6e1c4c6ce274b75b2a93c
thomas.bouchardon <thomas.bouchardon@developpement-durable.gouv.fr>
2019-09-16 14:30:03 +0200

    Fix : NPE si pas de raison sociale en base

--- kelrisks-facade/src/main/java/fr/gouv/beta/fabnum/kelrisks/facade/mapping/refentiel/ISecteurInformationSolMapper.java
+++ kelrisks-facade/src/main/java/fr/gouv/beta/fabnum/kelrisks/facade/mapping/refentiel/ISecteurInformationSolMapper.java
@@ -11,19 +11,20 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.apache.commons.lang3.StringUtils;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 
 @Mapper(config = ICommonMapperConfig.class,
-        imports = {GeoJsonUtils.class, Map.class, Collectors.class, Stream.class, AbstractMap.class})
+        imports = {GeoJsonUtils.class, Map.class, Collectors.class, Stream.class, AbstractMap.class, StringUtils.class})
 public interface ISecteurInformationSolMapper extends IGeometryMapper {
     
     List<SecteurInformationSolDTO> toDTOs(List<SecteurInformationSol> secteurInformationSolList);
     
     //    @formatter:off
     @Mappings(
-            @Mapping(target = "ewkt", expression = "java(GeoJsonUtils.toGeoJson(secteurInformationSol.getMultiPolygon(), Stream.of(new AbstractMap.SimpleEntry<>(\"Numéro\", secteurInformationSol.getNumero())).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue))))")
+            @Mapping(target = "ewkt", expression = "java(GeoJsonUtils.toGeoJson(secteurInformationSol.getMultiPolygon(), Stream.of(new AbstractMap.SimpleEntry<>(\"Numéro\", StringUtils.stripToEmpty(secteurInformationSol.getNumero()))).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue))))")
     )
     //    @formatter:on
     SecteurInformationSolDTO toDTOs(SecteurInformationSol secteurInformationSol);
